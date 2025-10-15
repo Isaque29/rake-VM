d7840b51b1ca1736dad9a2b17d2a5bb6e2ddf705
isaque <isaque.leite.sales@gmail.com>
2025-10-15 17:10:11 -0400

    better display message

--- src/parsing/parser.nim
+++ src/parsing/parser.nim
@@ -1,8 +1,12 @@
 import ../lexing/tokenizer
 import ../common/token
+import ../common/kind
 
 proc parse* (src: string): void =
     var tz: Tokenizer = newTokenizer(src)
     var toks: seq[Token] = tz.tokenize()
     for t in toks:
+        if t.kind == tkNewLine:
+            echo '\n'
+            continue
         echo t.line, ":", t.col, " ", $t.kind, " -> '", t.lexeme, "'"
